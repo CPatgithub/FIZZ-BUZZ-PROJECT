2acb8b6410a261570deb4aa8dbdbe119c2e15314
C P Singh <120619302+CPatgithub@users.noreply.github.com>
2023-03-07 09:21:28 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,17 @@ import React from 'react'
 import '../styles/App.css';
 const App = () => {
 
+  const [count, setCount] = useState(1);
+  let className = "normal"
+  if(count%3===0){className="fizz"}
+  if(count%5===0){className="buzz"}
+  if(count%3===0 && count%5===0){className="fizzbuzz"}
   
   return (
     <div id="main">
-
+      <button id ="increment" onClick={()=>{setCount(count+1);}}> increment </button>
+      <div id="counter">{count}</div>
+      <button id="decrement" onClick={()=>{setCount(count-1);}}> decrement </button>
     </div>
   )
 }
